3dff140c010e00b0aa4580e80ec12c016fb6097a
Felipe Menegat <felipe_menegat@hotmail.com>
2018-06-24 13:04:55 -0300

    Add files via upload

--- dbmecGustavo.sql
+++ dbmecGustavo.sql
@@ -13,15 +13,36 @@ senha varchar(30) not null
 create table tbclientes(
 idcli int primary key not null auto_increment,
 nomecli varchar(40) not null,
-cpfcli varchar(14) not null,
+cpfcli varchar(14) not null unique,
 enderecocli varchar(50) not null,
-emailcli varchar(50) not null,
-senhacli varchar(15) not null,
-placaveicli varchar(10),
-veiculocli varchar(14),
-kmveicli varchar(9999)  
+cidadecli varchar(30) not null,
+fonecli varchar(14) not null,
+emailcli varchar(50) not null
 );
 
 insert into tbeusuario(nomeuser, emailuser, login, senha)
 values ('Admin', 'admin@admin.com', 'admin', 'admin');
-describe tbclientes;
+describe tbos;
+select * from tbclientes;
+-- a linha abaixo adiciona um campo na tabela
+alter table tbeusuario add column perfil varchar(20) not null;
+-- a linha abaixo remove um campo da tabela 
+alter table tbeusuario drop column perfil;
+update tbeusuario set perfil='admin' where iduser=1;
+select * from tbeusuario where iduser=1;
+
+alter table tbeusuario 
+add logincli varchar(30) not null unique;
+
+create table tbos(
+os int(11) not null primary key auto_increment,
+data_os timestamp not null,
+veiculo varchar(100) not null,
+placa varchar(100) not null,
+km varchar(10000) not null, 
+problema varchar(300),
+servico_executado varchar(100),
+mecanico varchar(50),
+fk_idcli int not null,
+foreign key (fk_idcli) references tbclientes (idcli) 
+);
